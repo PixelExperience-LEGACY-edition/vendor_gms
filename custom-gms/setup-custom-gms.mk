213b90e47b6f8c1b2c98a58509c8a782572aeac1
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-06-17 13:37:51 +0000

    custom-gms: Add a feature permission for Call recording
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- custom-gms/setup-custom-gms.mk
+++ custom-gms/setup-custom-gms.mk
@@ -4,3 +4,8 @@ PRODUCT_PACKAGES += \
 	YouTube \
 	PrebuiltGmsCoreSc \
 	Velvet
+
+# Permissions for Call recording
+PRODUCT_COPY_FILES += \
+    vendor/partner-gms/custom-gms/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
